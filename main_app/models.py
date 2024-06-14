d32ab778bd15bf63bd3d1d9ee91cf033307b45bf
mastaneh8998 <131448895+mastaneh8998@users.noreply.github.com>
2024-06-14 21:13:24 +0330

    Update models.py

--- main_app/models.py
+++ main_app/models.py
@@ -24,16 +24,3 @@ class post(models.Model):
     def like_count(self):
         return self.like_set.count()
 
-class Like(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    post = models.ForeignKey(post, on_delete=models.CASCADE)
-    class Meta:
-        unique_together = ('user', 'post')
-    
-class Comment(models.Model):
-    post = models.ForeignKey(post, related_name='comments', on_delete=models.CASCADE)
-    user_name = models.CharField(max_length=255 , blank=False)
-    text = models.TextField(blank=True)
-    email = models.EmailField(default="emailaddress@gmail.com")
-    created_time = jmodels.jDateTimeField(auto_now_add=True)
-
